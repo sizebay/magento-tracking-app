d4679d0205bc3cf1c62017d07c6c1c155f48505d
amorim-dm <pedrowamorim@gmail.com>
2024-06-12 17:01:58 +0100

    Initial Commit

---
+++ SizeBay/SizeBayTracker/Observer/SizebaySessionId.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace SizeBay\SizeBayTracker\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Psr\Log\LoggerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class SizebaySessionId implements ObserverInterface
+{
+    protected $logger;
+    protected $scopeConfig;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    private function requestSessionID($retryCount = 3)
+    {
+        $session_url = "https://vfr-v3-production.sizebay.technology/api/me/session-id";
+
+        for ($i = 0; $i < $retryCount; $i++) {
+            $session_request = curl_init($session_url);
+            curl_setopt($session_request, CURLOPT_RETURNTRANSFER, true);
+
+            $response = curl_exec($session_request);
+            $httpcode = curl_getinfo($session_request, CURLINFO_HTTP_CODE);
+            $error = curl_error($session_request);
+
+            if ($response === false) {
+                $errno = curl_errno($session_request);
+                curl_close($session_request);
+                throw new \Exception(sprintf('Response code: %s. Error: %s', $httpcode, $error), $errno);
+            }
+
+            curl_close($session_request);
+
+            $cookieSet = setcookie('SIZEBAY_SESSION_ID_V4', json_decode($response), [
+                'expires' => time() + 3600,
+                'path' => '/',
+                'secure' => isset($_SERVER['HTTPS']),
+                'httponly' => true,
+                'samesite' => 'Lax',
+            ]);
+
+            if (!$cookieSet) {
+                throw new \Exception('Failed to set the session ID cookie.');
+            }
+
+
+            sleep(1);
+        }
+
+        throw new \Exception('Invalid response format. Expected JSON object.');
+    }
+
+
+    private function requestSessionIdValidation()
+    {
+        if (!isset($_COOKIE["SIZEBAY_SESSION_ID_V4"])) {
+            $this->requestSessionID();
+
+            if (!isset($_COOKIE["SIZEBAY_SESSION_ID_V4"])) {
+                throw new \Exception('Session ID cookie not set after attempting to generate one');
+            }
+        }
+    }
+
+
+    public function execute(Observer $observer)
+    {
+        try {
+            $this->requestSessionIdValidation();
+        } catch (\Exception $e) {
+            $this->logger->error('Error in SizeBayTracker observer: ' . $e->getMessage());
+        }
+    }
+}
---
+++ SizeBay/SizeBayTracker/Observer/SizebayTrackerOrder.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace SizeBay\SizeBayTracker\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Psr\Log\LoggerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class SizebayTrackerOrder implements ObserverInterface
+{
+    protected $logger;
+    protected $scopeConfig;
+
+    public function __construct(LoggerInterface $logger, ScopeConfigInterface $scopeConfig)
+    {
+        $this->logger = $logger;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    private function executeOrderPluginRequest($order)
+    {
+        try {
+            $tenant_id = strval($this->scopeConfig->getValue('sizebay_sizebaytracker/settings/tenant_id', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+            $referer = strval($this->scopeConfig->getValue('sizebay_sizebaytracker/settings/referer', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+            $country = strval($this->scopeConfig->getValue('sizebay_sizebaytracker/settings/country', \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
+
+            $sessionId = $_COOKIE["SIZEBAY_SESSION_ID_V4"];
+            $url = "https://vfr-v3-production.sizebay.technology/plugin/new/ordered?sid=" . $sessionId;
+
+            $items = [];
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
+
+            foreach ($order->getAllVisibleItems() as $item) {
+                $product = $item->getProduct();
+                $productOptions = $item->getProductOptions();
+                $size = '';
+
+                $sizeAttributeId = $this->scopeConfig->getValue(
+                    'sizebay_sizebaytracker/settings/size_attribute',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                );
+
+                // If the size attribute is configured
+                if (!empty($sizeAttributeId)) {
+                    if (isset($productOptions['attributes_info'])) {
+                        foreach ($productOptions['attributes_info'] as $attributeInfo) {
+                            // Check if the attribute matches the configured size attribute
+                            if ($attributeInfo['option_id'] == $sizeAttributeId) {
+                                $size = $attributeInfo['value'];
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                isset($size) ? $size : 'N/A';
+
+                $items[] = [
+                    "permalink" => $storeManager->getStore()->getBaseUrl() . $product->getId(),
+                    "price" => $item->getPrice(),
+                    "quantity" => intval($item->getQtyOrdered()),
+                    "size" => $size,
+                    "feedProductId" => $product->getId(),
+                    "sku" => $item->getSku()
+                ];
+            }
+
+
+            $data = [
+                "orderId" => $order->getId(),
+                "items" => $items,
+                "tenantId" => $tenant_id,
+                "currency" => $order->getOrderCurrencyCode(),
+                "country" => $country,
+            ];
+
+            $ch = curl_init($url);
+
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'content-type: application/json',
+                'accept: application/json',
+                'device: DESKTOP',
+                'tenant_id: ' . $tenant_id,
+                'referer: ' . $referer,
+            ]);
+
+            $response = curl_exec($ch);
+
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            $error = curl_error($ch);
+            if ($response === false) {
+                $errno = curl_errno($ch);
+                curl_close($ch);
+                throw new \Exception(sprintf('Response code: %s. Error: %s', (string) $httpcode, $error), $errno);
+            }
+
+            $response = json_decode($response);
+
+            curl_close($ch);
+        } catch (\Exception $e) {
+            $this->logger->error('Error in SizeBayTracker fetch: ' . $e->getMessage());
+        }
+    }
+
+
+    public function execute(Observer $observer)
+    {
+        $order = $observer->getEvent()->getOrder();
+        try {
+            $this->executeOrderPluginRequest($order);
+        } catch (\Exception $e) {
+            $this->logger->error('Error in SizeBayTracker observer: ' . $e->getMessage());
+        }
+    }
+
+}
