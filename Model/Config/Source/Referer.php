1662d8514c042086709674fe11f7a7b75c730329
amorim-dm <pedrowamorim@gmail.com>
2024-06-18 18:54:03 +0100

    Referer Correction

---
+++ Model/Config/Source/Referer.php
@@ -0,0 +1,40 @@
+<?php
+namespace Sizebay\SizebayTracker\Model\Config\Backend;
+
+use Magento\Framework\App\Config\Value;
+use Magento\Store\Model\StoreManagerInterface;
+
+class Referer extends Value
+{
+    protected $storeManager;
+
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool,
+        $data = []
+    ) {
+        $this->storeManager = $storeManager;
+        parent::__construct(
+            $context,
+            $registry,
+            $config,
+            $cacheTypeList,
+            $cacheFrontendPool,
+            $data
+        );
+    }
+
+    public function afterLoad()
+    {
+        if ($this->getValue() === null) {
+            $baseUrl = $this->storeManager->getStore()->getBaseUrl();
+            $domain = parse_url($baseUrl, PHP_URL_HOST);
+            $this->setValue($domain);
+        }
+        return parent::afterLoad();
+    }
+}
