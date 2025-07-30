1d54bb52e0a704a6ccf581e56b4e2cb90dd42f69
amorim-dm <pedrowamorim@gmail.com>
2025-07-30 14:41:43 +0100

    PHPDoc block

--- Api/Data/CartAddInterface.php
+++ Api/Data/CartAddInterface.php
@@ -1,9 +1,37 @@
 <?php
 namespace Sizebay\SizebayTracker\Api\Data;
 
-interface CartAddInterface {
-    public function getItems(); // define needed getters
+/**
+ * Interface CartAddInterface
+ * Represents the structure of a cart addition tracking message.
+ */
+interface CartAddInterface
+{
+    /**
+     * Get the items added to the cart.
+     *
+     * @return array
+     */
+    public function getItems();
+
+    /**
+     * Get the session ID at the time items were added.
+     *
+     * @return string
+     */
     public function getSessionId();
+
+    /**
+     * Get the tenant ID related to the session.
+     *
+     * @return string
+     */
     public function getTenantId();
+
+    /**
+     * Get the referer information.
+     *
+     * @return string
+     */
     public function getReferer();
 }
--- Api/Data/OrderTrackInterface.php
+++ Api/Data/OrderTrackInterface.php
@@ -1,13 +1,58 @@
 <?php
 namespace Sizebay\SizebayTracker\Api\Data;
 
+/**
+ * Interface OrderTrackInterface
+ * Represents the structure of an order tracking message.
+ */
 interface OrderTrackInterface
 {
+    /**
+     * Get the unique order ID.
+     *
+     * @return string
+     */
     public function getOrderId();
+
+    /**
+     * Get the items included in the order.
+     *
+     * @return array
+     */
     public function getItems();
+
+    /**
+     * Get the tenant ID related to the order.
+     *
+     * @return string
+     */
     public function getTenantId();
+
+    /**
+     * Get the referer information.
+     *
+     * @return string
+     */
     public function getReferer();
+
+    /**
+     * Get the session ID at the time of the order.
+     *
+     * @return string
+     */
     public function getSessionId();
+
+    /**
+     * Get the currency used for the order.
+     *
+     * @return string
+     */
     public function getCurrency();
+
+    /**
+     * Get the country from where the order was made.
+     *
+     * @return string
+     */
     public function getCountry();
 }
