c62e0b8fdc7b718d19f675c21a953d85780ebdd2
amorim-dm <pedrowamorim@gmail.com>
2025-07-30 17:16:44 +0100

    Trying Again 4

--- Api/Data/CartAddInterface.php
+++ Api/Data/CartAddInterface.php
@@ -1,17 +1,67 @@
 <?php
 namespace Sizebay\SizebayTracker\Api\Data;
 
-interface CartAddInterface
+use Magento\Framework\Api\ExtensibleDataInterface;
+
+interface CartAddInterface extends ExtensibleDataInterface
 {
+    /**
+     * Get items
+     *
+     * @return array|null
+     */
     public function getItems();
+
+    /**
+     * Set items
+     *
+     * @param array $items
+     * @return $this
+     */
     public function setItems(array $items);
 
+    /**
+     * Get session ID
+     *
+     * @return string|null
+     */
     public function getSessionId();
+
+    /**
+     * Set session ID
+     *
+     * @param string $sessionId
+     * @return $this
+     */
     public function setSessionId($sessionId);
 
+    /**
+     * Get tenant ID
+     *
+     * @return string|null
+     */
     public function getTenantId();
+
+    /**
+     * Set tenant ID
+     *
+     * @param string $tenantId
+     * @return $this
+     */
     public function setTenantId($tenantId);
 
+    /**
+     * Get referer
+     *
+     * @return string|null
+     */
     public function getReferer();
+
+    /**
+     * Set referer
+     *
+     * @param string $referer
+     * @return $this
+     */
     public function setReferer($referer);
 }
--- Api/Data/OrderTrackInterface.php
+++ Api/Data/OrderTrackInterface.php
@@ -1,26 +1,112 @@
 <?php
 namespace Sizebay\SizebayTracker\Api\Data;
 
-interface OrderTrackInterface
+use Magento\Framework\Api\ExtensibleDataInterface;
+
+interface OrderTrackInterface extends ExtensibleDataInterface
 {
+    /**
+     * Get order ID
+     *
+     * @return string|null
+     */
     public function getOrderId();
+
+    /**
+     * Set order ID
+     *
+     * @param string $orderId
+     * @return $this
+     */
     public function setOrderId($orderId);
 
+    /**
+     * Get items
+     *
+     * @return array|null
+     */
     public function getItems();
+
+    /**
+     * Set items
+     *
+     * @param array $items
+     * @return $this
+     */
     public function setItems(array $items);
 
+    /**
+     * Get tenant ID
+     *
+     * @return string|null
+     */
     public function getTenantId();
+
+    /**
+     * Set tenant ID
+     *
+     * @param string $tenantId
+     * @return $this
+     */
     public function setTenantId($tenantId);
 
+    /**
+     * Get referer
+     *
+     * @return string|null
+     */
     public function getReferer();
+
+    /**
+     * Set referer
+     *
+     * @param string $referer
+     * @return $this
+     */
     public function setReferer($referer);
 
+    /**
+     * Get session ID
+     *
+     * @return string|null
+     */
     public function getSessionId();
+
+    /**
+     * Set session ID
+     *
+     * @param string $sessionId
+     * @return $this
+     */
     public function setSessionId($sessionId);
 
+    /**
+     * Get currency
+     *
+     * @return string|null
+     */
     public function getCurrency();
+
+    /**
+     * Set currency
+     *
+     * @param string $currency
+     * @return $this
+     */
     public function setCurrency($currency);
 
+    /**
+     * Get country
+     *
+     * @return string|null
+     */
     public function getCountry();
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return $this
+     */
     public function setCountry($country);
 }
