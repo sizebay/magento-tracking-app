d544f7b46eb1593a0640de710451049b8918c31f
amorim-dm <pedrowamorim@gmail.com>
2025-08-22 19:08:24 +0100

    OrderItem Correction

--- Observer/SizebayTrackerOrder.php
+++ Observer/SizebayTrackerOrder.php
@@ -40,72 +40,67 @@ class SizebayTrackerOrder implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        try {
-            if (!$this->isModuleActive()) {
-                return;
-            }
+        if (!$this->isModuleActive()) {
+            return;
+        }
 
-            $order = $observer->getEvent()->getOrder();
+        $order = $observer->getEvent()->getOrder();
 
-            $tenantId = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/tenant_id');
-            $referer = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/referer');
-            $country = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/country');
-            $sizeAttributeId = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/size_attribute');
-            $sessionId = $_COOKIE['SIZEBAY_SESSION_ID_V4'] ?? '';
+        $tenantId = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/tenant_id');
+        $referer = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/referer');
+        $country = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/country');
+        $sizeAttributeId = $this->scopeConfig->getValue('sizebay_sizebaytracker/settings/size_attribute');
+        $sessionId = $_COOKIE['SIZEBAY_SESSION_ID_V4'] ?? '';
 
-            $items = [];
+        $items = [];
 
-            foreach ($order->getAllVisibleItems() as $item) {
-                $product = $item->getProduct();
-                $productOptions = $item->getProductOptions();
+        foreach ($order->getAllVisibleItems() as $item) {
+            $product = $item->getProduct();
+            $productOptions = $item->getProductOptions();
 
-                $size = 'N/A';
-                if (!empty($sizeAttributeId) && isset($productOptions['attributes_info'])) {
-                    foreach ($productOptions['attributes_info'] as $attr) {
-                        if ($attr['option_id'] == $sizeAttributeId) {
-                            $size = $attr['value'];
-                            break;
-                        }
+            $size = 'N/A';
+            if (!empty($sizeAttributeId) && isset($productOptions['attributes_info'])) {
+                foreach ($productOptions['attributes_info'] as $attr) {
+                    if ($attr['option_id'] == $sizeAttributeId) {
+                        $size = $attr['value'];
+                        break;
                     }
-                }
-
-                try {
-                    $orderItem = new OrderItem();
-                    $orderItem->setSku($item->getSku());
-                    $orderItem->setQuantity((int)$item->getQtyOrdered());
-                    $orderItem->setPrice((float)$item->getPrice());$permalink = $this->storeManager->getStore()->getBaseUrl() . $product->getId();
-                    $orderItem->setPermalink($permalink);
-                    $orderItem->setSize($size);
-                    $orderItem->setFeedProductId($product->getId());
-
-                    $items[] = $orderItem;
-                } catch (\Exception $e) {
-                    $this->logger->critical($e);
                 }
             }
 
             try {
-                $orderTrack = new OrderTrack();
-                $orderTrack->setOrderId($order->getId())
-                    ->setItems($items)
-                    ->setTenantId($tenantId)
-                    ->setReferer($referer)
-                    ->setSessionId($sessionId)
-                    ->setCurrency($order->getOrderCurrencyCode())
-                    ->setCountry($country);
+                $orderItem = new OrderItem();
+                $orderItem->setSku($item->getSku());
+                $orderItem->setQuantity((int)$item->getQtyOrdered());
+                $orderItem->setPrice((float)$item->getPrice());$permalink = $this->storeManager->getStore()->getBaseUrl() . $product->getId();
+                $orderItem->setPermalink($permalink);
+                $orderItem->setSize($size);
+                $orderItem->setFeedProductId($product->getId());
+
+                $items[] = $orderItem;
             } catch (\Exception $e) {
                 $this->logger->critical($e);
             }
-   
-            try {
-            $this->orderPublisher->publish($orderTrack);
-            $this->logger->info("Sizebay Order is published: " . json_encode($orderTrack));
-            } catch (Exception $e) {
-                $this->logger->error($e->getMessage());
-            }
+        }
 
+        try {
+            $orderTrack = new OrderTrack();
+            $orderTrack->setOrderId($order->getId())
+                ->setItems($items)
+                ->setTenantId($tenantId)
+                ->setReferer($referer)
+                ->setSessionId($sessionId)
+                ->setCurrency($order->getOrderCurrencyCode())
+                ->setCountry($country);
         } catch (\Exception $e) {
-            $this->logger->error('Error in SizebayTrackerOrder observer: ' . $e->getMessage());
+            $this->logger->critical($e);
+        }
+
+        try {
+        $this->orderPublisher->publish($orderTrack);
+        $this->logger->info("Sizebay Order is published: " . json_encode($orderTrack));
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage());
         }
     }
 }
