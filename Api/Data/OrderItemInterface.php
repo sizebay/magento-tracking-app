24e0570b5d7225b6bd8987c71cad2d74844b4b83
amorim-dm <pedrowamorim@gmail.com>
2025-08-23 12:32:21 +0100

    OrderItem Correction

--- Api/Data/OrderItemInterface.php
+++ Api/Data/OrderItemInterface.php
@@ -5,21 +5,93 @@ use Magento\Framework\Api\ExtensibleDataInterface;
 
 interface OrderItemInterface extends ExtensibleDataInterface
 {
-    public function getSku(): ?string;
-    public function setSku(string $sku): self;
+    /**
+     * Get product SKU
+     *
+     * @return string|null
+     */
+    public function getSku();
 
-    public function getQuantity(): ?int;
-    public function setQuantity(int $qty): self;
+    /**
+     * Set product SKU
+     *
+     * @param string $sku
+     * @return $this
+     */
+    public function setSku($sku);
 
-    public function getPrice(): ?float;
-    public function setPrice(float $price): self;
+    /**
+     * Get quantity
+     *
+     * @return int|null
+     */
+    public function getQuantity();
 
-    public function getPermalink(): ?string;
-    public function setPermalink(string $permalink): self;
+    /**
+     * Set quantity
+     *
+     * @param int $qty
+     * @return $this
+     */
+    public function setQuantity($qty);
 
-    public function getSize(): ?string;
-    public function setSize(string $size): self;
+    /**
+     * Get price
+     *
+     * @return float|null
+     */
+    public function getPrice();
 
-    public function getFeedProductId(): ?int;
-    public function setFeedProductId(int $id): self;
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return $this
+     */
+    public function setPrice($price);
+
+    /**
+     * Get permalink
+     *
+     * @return string|null
+     */
+    public function getPermalink();
+
+    /**
+     * Set permalink
+     *
+     * @param string $permalink
+     * @return $this
+     */
+    public function setPermalink($permalink);
+
+    /**
+     * Get size
+     *
+     * @return string|null
+     */
+    public function getSize();
+
+    /**
+     * Set size
+     *
+     * @param string $size
+     * @return $this
+     */
+    public function setSize($size);
+
+    /**
+     * Get feed product ID
+     *
+     * @return int|null
+     */
+    public function getFeedProductId();
+
+    /**
+     * Set feed product ID
+     *
+     * @param int $id
+     * @return $this
+     */
+    public function setFeedProductId($id);
 }
